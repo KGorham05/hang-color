edf50d1def0edd3b90ee0257ae5770bec4e3d398
Kgor <kevin.gorham@gmail.com>
2019-08-26 19:08:47 -0700

    started building game logic

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+// Create an object to hold the game logic and variables
+var wordGuessGame = {
+
+    // initialize all game variables
+    wordInPlay: null,
+    lettersOfTheWord: [],
+    matchedLetters: [],
+    guessedLetters: [],
+    guessesLeft: 0,
+    totalGuesses: 0,
+    letterGuessed: null,
+    wins: 0,
+
+    wordsToPick: {
+        red: {
+            hex: 
+        }
+    }
+
+}
