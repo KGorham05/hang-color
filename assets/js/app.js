35e02c134233bbd62ab5983969c53f02024b70a2
Kgor <kevin.gorham@gmail.com>
2019-08-28 10:09:23 -0700

    working restartGame function

--- assets/js/app.js
+++ assets/js/app.js
@@ -65,26 +65,26 @@ var wordGuessGame = {
         },
         navy: {
             hex: '000080'
-        }
+        },
 
         // add brown white and grey colors
         // https://htmlcolorcodes.com/color-names/
     },
 
     // call this function when the page first loads. 
-    setupGame: function() {
+    setupGame: function () {
         // pick a random word
         var objectKeys = Object.keys(this.wordsToPick);
         this.wordInPlay = objectKeys[Math.floor(Math.random() * objectKeys.length)];
         // split the word into individual letters
         this.lettersOfTheWord = this.wordInPlay.split("");
         // update background color css to wordInPlay
-        
+
     },
-    
+
     // call this function whenever a user guesses a letter...
-    updatePage: function(letter) {
-        // listen for keypress 
+    updatePage: function (letter) {
+
         // if the user has no guesses left, restart the game
         //else
         // check for and handle incorrect guesses
@@ -92,37 +92,45 @@ var wordGuessGame = {
         // rebuild the view of the word - 
         // guessed letters are revealed, non guessed letters have a "_"
     },
-    
+
     // This function sets the initial guesses the user gets
-    setInitialGuesses: function() {
+    setInitialGuesses: function () {
         // set number of guesses equal to letters in the word + 5
         // display number of guesses on a page
-        
+
     },
 
     // run this func when a user guesses a wrong letter
-    updateGuesses: function(letter) {
+    updateGuesses: function (letter) {
 
     },
 
     // run this function when a user guesses correctly
-    updateMatchedLetters: function(letter) {
+    updateMatchedLetters: function (letter) {
 
     },
 
     // builds the display of the word currently being guessed
     // ex. if the color is "salmon" it might display "S_lm_ _"
-    rebuildWordView: function() {
+    rebuildWordView: function () {
 
     },
 
     // function that restarts the game by resetting all of the variables. 
-    restartGame: function() {
+    restartGame: function () {
         // reset global variables 
+        this.wordInPlay = null,
+        this.lettersOfTheWord = [],
+        this.matchedLetters = [],
+        this.guessedLetters = [],
+        this.guessesLeft = 0,
+        this.totalGuesses = 0,
+        this.letterGuessed = null,
+        this.wins = 0,
     },
 
     // function that checks to see if the user has won the game
-    updateWins: function() {
+    updateWins: function () {
 
     },
 
@@ -132,7 +140,7 @@ var wordGuessGame = {
 wordGuessGame.setupGame();
 
 // listen for keypress
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     // check if the key pressed is a letter
     if (event.keyCode >= 49 && event.keyCode <= 90) {
         //capture the key pressed and make it lowercase
